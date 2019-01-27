59d969c2ec42b036efdc7c56c0df47e0f13bcecf
JanaBala <janardhandmm2@gmail.com>
2019-01-27 20:41:17 +0530

    Code Modified with Selenium Project

--- src/test/java/com/demo/test/K_AutoTest.java
+++ src/test/java/com/demo/test/K_AutoTest.java
@@ -14,65 +14,53 @@ import org.testng.annotations.Test;
 
 public class K_AutoTest {
 
-public WebDriver driver;
-	
-	@BeforeMethod	 
+	public WebDriver driver;
+
+	@BeforeMethod
 	public void beforeMethod() {
-	 
+
 		// Create a new instance of the Firefox driver
 		System.out.println("launching Chrome browser");
-	
-		System.setProperty("webdriver.chrome.driver",
-				"..//Kemper//src//test//resources//Drivers//chromedriver.exe");
-		
-	    driver = new ChromeDriver();
-	 
-	      //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
-	 
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	 
-	      //Launch the Online Store Website
-	 
-	    driver.get("https://www1int.kemper.com/agencyportal/");
-	 
-	  }
-	
-	@Test	 
-	 public void Login() throws Exception {
-	 
-	      driver.findElement(By.name("USER")).sendKeys("B0032308");
-		 
-	      driver.findElement(By.name("PASSWORD")).sendKeys("Kemper19!");
-	 
-	      driver.findElement(By.id("button")).click();
-	 
-	      System.out.println(" Login Successfully");
-	 
-	      Thread.sleep(10000);
-	      
-	      driver.findElement(By.id("menu-entry-get-started")).click();
-	      Thread.sleep(20000);
-	      driver.findElement(By.xpath("//*[@id='main-menu']/div/div[2]/ul[2]/li/ul/li[2]/a")).click();
-	      Thread.sleep(20000);
-	     
-	      WebElement Producer = driver.findElement(By.id("AgencyProdCode"));
-	      Select s=new Select(Producer);
-	      List<WebElement> allvals = s.getOptions();
-	      
-	      for(int i=0;i<allvals.size();i++)
-	      {
-	       Thread.sleep(1000);
-	       //System.out.println("CheckBox:"+ allvals.get(i).getAttribute("value"));
-	       s.selectByIndex(i); //select one by one
-	      }
-	      
-	  }
-	 
-	 @AfterMethod	 
-	 public void afterMethod() {
-	 
-		  // Close the driver	 
-	      driver.quit();
-	 
-	  }
+
+		System.setProperty("webdriver.chrome.driver", "..//Kemper//src//test//resources//Drivers//chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		// Put a Implicit wait, this means that any search for elements on the
+		// page could take the time the implicit wait is set for before throwing
+		// exception
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		// Launch the Online Store Website
+
+		driver.get("https://learn.letskodeit.com/p/practice");
+
+	}
+
+	@Test
+	public void Login() throws Exception {
+
+		Thread.sleep(5000);
+
+		driver.findElement(By.name("USER")).click();
+		driver.findElement(By.name("USER")).clear();
+		driver.findElement(By.name("USER")).sendKeys("B0032308");
+		driver.findElement(By.name("PASSWORD")).clear();
+		driver.findElement(By.name("PASSWORD")).sendKeys("Kemper15");
+		driver.findElement(By.id("button")).click();
+
+		System.out.println(" Login Successfully");
+
+		Thread.sleep(10000);
+
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+
+		// Close the driver
+		driver.quit();
+
+	}
 }
